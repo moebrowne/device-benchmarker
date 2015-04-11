985a45cfced087df689e5d834f828332638d9189
MoeBrowne <oliver.browne@uplyme.com>
2015-04-11 14:12:21 +0000

    Added mount-device function that mounts a single device to a new mount point, if needed

--- libs/mount-devices.sh
+++ libs/mount-devices.sh
@@ -5,3 +5,29 @@ function mount-devices {
 	mount -t ext4 -o defaults /dev/sda1 /media/black
 	mount -t ext4 -o defaults /dev/sdb1 /media/orange
 }
+
+function mount-device {
+	# Check if the device is already mounted
+	if [ $(mount | grep -c "$1") != 1 ]; then
+		echo "Device $1 is already mounted. Moving on..."
+		exit
+	fi
+	
+	# Get the device name
+	DEVICE_NAME=$(basename "$1")
+	
+	# Set the mount point directory
+	DEVICE_MOUNTPOINT="/media/$DEVICE_NAME"
+	
+	# Check if a mount directory exists for this device
+	if [ ! -d "$DEVICE_MOUNTPOINT" ]; then
+		# Create the mount point
+		echo "Creating the mount point directory"
+		mkdir -p "$DEVICE_MOUNTPOINT"
+	fi
+	
+	# Mount the device
+	echo "Mounting $1 to $DEVICE_MOUNTPOINT"
+	mount $1 $DEVICE_MOUNTPOINT
+	
+}
