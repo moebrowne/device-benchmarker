904dff2653d1e0fb1514c4b27ab45bc49e8ff67b
MoeBrowne <oliver.browne@uplyme.com>
2015-04-11 15:16:30 +0000

    Set the benchmarker to run only the requested test

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 # Librarys
 . /home/pi/benchmark/libs/devices.sh
+. /home/pi/benchmark/tests/dd.sh
 
 # Parameters
 # $1		: Type of test to run
@@ -12,7 +13,7 @@ TEST="$1"
 DEVICES="${@: 2}"
 
 # Mount all the devices
-for device in "$DEVICES"; do
+for device in $DEVICES; do
 	device-mount "$device"
 done
 
@@ -21,9 +22,6 @@ echo "Clearning caches and buffers"
 sync && sync
 echo 3 > /proc/sys/vm/drop_caches
 
-
-
-echo "DD: 1M Blocks | 512 Blocks | 512MB"
-dd bs=1M if=/dev/zero of=/media/orange/zeros count=512 2> black &
-dd bs=1M if=/dev/zero of=/media/black/zeros count=512 2> orange &
-
+if [ "$TEST" == "dd" ]; then
+	test-dd $DEVICES
+fi
